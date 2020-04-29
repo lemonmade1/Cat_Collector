bf97753dcf13aa9da8523abdfabf0149121ba659
Terrell D Lemons <terrell@tmcompmc16.home>
2020-04-29 08:28:46 -0700

    Django, Day 2, complete

--- catcollector/urls.py
+++ catcollector/urls.py
@@ -10,15 +10,13 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+  1. Import the include() function: from django.urls import include, path
+  2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# Add the include function to the import
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # In this case '' represents the root route
-    path('', include('main_app.urls')),
+  path('admin/', admin.site.urls),
+  path('', include('main_app.urls')),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
+from .models import Cat
 
-class Cat:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+# class Cat(models.Model):
+#   name = models.CharField(max_length=100)
+#   breed = models.CharField(max_length=100)
+#   description = models.TextField(max_length=250)
+#   age = models.IntegerField()
 
-cats = [
-  Cat('Lolo', 'Tabby', 'foul little demon', 3),
-  Cat('Sachi', 'Tortoise Shell', 'Diluted Tortoise Shell', 0),
-  Cat('Raven', 'Black Tripod', '3 legged cat', 4)
-]
+# def __str__(self):
+#   return self.name
+
+# cats = [
+#   Cat('Lolo', 'Tabby', 'foul little demon', 3),
+#   Cat('Sachi', 'Tortoise Shell', 'Diluted Tortoise Shell', 0),
+#   Cat('Raven', 'Black Tripod', '3 legged cat', 4)
+# ]
 
 # Define the home view
 def home(request):
@@ -21,8 +24,9 @@ def about(request):
   return render(request, 'about.html')
 
 def cats_index(request):
-  return render(request, 'cats/index.html', {
-		'cats': cats
-	})
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', { 'cats': cats })
 
-
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', { 'cat': cat })
