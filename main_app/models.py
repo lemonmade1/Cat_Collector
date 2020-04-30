581a80025b101886cdc82c22e8bca3745e071d0b
Terrell D Lemons <terrell@tmcompmc16.home>
2020-04-30 10:33:00 -0700

    made migrations: addedCat foreign key relationship

--- main_app/models.py
+++ main_app/models.py
@@ -32,6 +32,9 @@ class Feeding(models.Model):
     default=MEALS[0][0]
   )
 
+  # Create a cat_id FK
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
 def __str__(self):
   # Nice method for obtaining the friendly value of a Field.choice
   return f"{self.get_meal_display()} on {self.date}"
